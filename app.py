282eca16faf26a4ff42ffc0df17f5e7ad02e826d
ferrichy29 <72745961+ferrichy29@users.noreply.github.com>
2023-05-21 17:32:44 +0700

    app.py

--- app.py
+++ app.py
@@ -99,8 +99,7 @@ def main():
        """)
 
        a,b,c = st.columns([0.2,0.6,0.2])
-with b:
- st.image("banner-picture.jpeg", use_column_width=True)
+with b: st.image("banner-picture.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
